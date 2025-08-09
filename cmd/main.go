53cbe667f06dc1809bee3b4837f32b07e6e0d354
dlworhd <dlwodud821@gmail.com>
2025-08-10 04:05:15 +0900

    feat: 웹훅 기능 추가(디스코드)

--- cmd/main.go
+++ cmd/main.go
@@ -10,11 +10,10 @@ import (
 func main() {
 	godotenv.Load("../.env.local")
 	log_level := os.Getenv("LOG_LEVEL")
+	discord_webhook_url := os.Getenv("DISCORD_WEBHOOK_URL")
 
 	xlog.Default(log_level)
+	xlog.AddWebhook(xlog.WebhookClient{Name: "DISCORD", Url: discord_webhook_url})
 
-	xlog.Debug("DEBUG TEST")
-	xlog.Info("INFO TEST")
-	xlog.Warn("WARN TEST")
-	xlog.Error("ERROR TEST")
+	xlog.Info("LOG TEST")
 }
